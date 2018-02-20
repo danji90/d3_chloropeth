c8f29ad86825837cf0cfebb06a1508bd43e83d66
danji_ma90 <danji_ma90_ma90@hotmail.com>
2018-02-20 18:01:42 +0100

    colours and popups working

--- core.js
+++ core.js
@@ -3,67 +3,62 @@
 var w = 1000;
 var h = 600;
 
-var color = [0xfff5f0,0xfee0d2,0xfcbba1,0xfc9272,0xfb6a4a,0xef3b2c,0xcb181d,0xa50f15,0x67000d].map(function(x) {
-    var value = x + "";
-    return d3.rgb(value >> 16, value >> 8 & 0xff, value & 0xff).toString();
-});
+// color options (reverse colours with reverse())
+// fading green: ["rgb(247,252,253)", "rgb(229,245,249)", "rgb(204,236,230)", "rgb(153,216,201)", "rgb(102,194,164)", "rgb(65,174,118)", "rgb(35,139,69)", "rgb(0,109,44)", "rgb(0,68,27)"]
+// purple fade to blueish: ["rgb(247,252,253)", "rgb(224,236,244)", "rgb(191,211,230)", "rgb(158,188,218)", "rgb(140,150,198)", "rgb(140,107,177)", "rgb(136,65,157)", "rgb(129,15,124)", "rgb(77,0,75)"]
+// red fading to yellow:
+// fading red
+
+var colorArray = ["rgb(247,252,253)", "rgb(224,236,244)", "rgb(191,211,230)", "rgb(158,188,218)", "rgb(140,150,198)", "rgb(140,107,177)", "rgb(136,65,157)", "rgb(129,15,124)", "rgb(77,0,75)"]
 
 
-// color options
-// red to blue: [0xb2182b,0xd6604d,0xf4a582,0xfddbc7,0xf7f7f7,0xd1e5f0,0x92c5de,0x4393c3,0x2166ac]
-// red fading to yellow (may have to reverse the array): [0xffffcc,0xffeda0,0xfed976,0xfeb24c,0xfd8d3c,0xfc4e2a,0xe31a1c,0xbd0026,0x800026]
-// fading red [0xfff5f0,0xfee0d2,0xfcbba1,0xfc9272,0xfb6a4a,0xef3b2c,0xcb181d,0xa50f15,0x67000d]
-
-var color = d3.scale.ordinal().range(color.reverse());
-
-// var color = d3.scale.quantize()
-//               .range(["rgb(237,248,233)", "rgb(186,228,179)",
-//                "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
-//
-
-//Define map projection
-// var projection = d3.geo.aitoff()
-// 					   .translate([w/2, h/2])
-//                               .scale([200]);
+var color = d3.scale.quantize()
+  .range(colorArray);
 
 
-var projection = d3.geo.winkel3().translate([w/2, h/2]).scale([200]);
+var projection = d3.geo.winkel3().translate([w / 2, h / 2]).scale([200]);
 
 
 //Define path generator
 var path = d3.geo.path()
-         .projection(projection);
+  .projection(projection);
 
 //Create SVG element
 var svg = d3.select("#map")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
 
 
 // Load productivity data
-d3.json("http://emotional-apps.com/apis/meit/stats/getdata.php?test=1&gender=all&age=all&begindate=2000-01-01&enddate=2018-02-04", function (data){
+d3.json("http://emotional-apps.com/apis/meit/stats/getdata.php?test=1&gender=all&age=all&begindate=2000-01-01&enddate=2018-02-04", function(data) {
 
   console.log(data)
   // Load colors for domain values
   color.domain([
-          d3.min(data, function(d) { return d.score_average; }),
-          d3.max(data, function(d) { return d.score_average; })
+    d3.min(data, function(d) {
+      return parseFloat(d.score_average);
+    }),
+    d3.max(data, function(d) {
+      return parseFloat(d.score_average);
+    })
   ]);
 
   // Load geometry and match state
 
   d3.json("http://pixel.uji.es/teaching/countries.geo.json.php", function(json) {
 
-  //Merge the ag. data and GeoJSON
-  //Loop through once for each ag. data value
-  for (var i = 0; i < data.length; i++) {
+
+    //Merge the ag. data and GeoJSON
+    //Loop through once for each ag. data value
+    for (var i = 0; i < data.length; i++) {
 
       //Grab state name
       var dataState = data[i].iso3;
 
       //Grab data value, and convert from string to float
       var dataValue = parseFloat(data[i].score_average);
+      var timeValue = parseFloat(data[i].time_average).toFixed(2);
 
       //Find the corresponding state inside the GeoJSON
       for (var j = 0; j < json.features.length; j++) {
@@ -73,61 +68,46 @@ d3.json("http://emotional-apps.com/apis/meit/stats/getdata.php?test=1&gender=all
         if (dataState == jsonState) {
           //Copy the data value into the JSON
           json.features[j].properties.score_average = dataValue;
+          json.features[j].properties.time_average = timeValue
           //Stop looking through the JSON
           break;
         }
       }
     }
-    console.log(json)
+
 
     var tip = d3.tip()
-           .attr('class', 'd3-tip')
-           .offset([-10, 0])
-           .html(function(d) {
-             return "<span>" + d.properties.name + "</span> </br> Score: <span>" + d.properties.score_average + "</span>";
-           });
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        return "<span>" + d.properties.name + "</span> </br> Score: <span>" + d.properties.score_average + "</span> </br> Time: <span>" + d.properties.time_average + "</span>";
+      });
     svg.call(tip);
 
     svg.selectAll("path")
-                 .data(json.features)
-                 .enter()
-                 .append("path")
-                 .attr("d", path)
-                 .style("fill", function(d) {
-                              //Get data value
-                              var score_average = d.properties.score_average;
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill", function(d) {
+        //Get data value
+        var score_average = d.properties.score_average;
 
-                              if (score_average) {
-                                      //If value exists...
-                                      return color(score_average);
-                              } else {
-                                      //If value is undefined...
-                                      return "#ccc";
-                              }
-                 })
-                 .on("mouseover", function(e) {
-         					d3.select(this).classed("highlight", true);
-                  tip.show(e)
-                  // console.log(e);
-                  // console.log(this)
-                 })
-         				 .on("mouseout", function(e) {
-         					d3.select(this).classed("highlight", false);
-                  tip.hide(e)
-         				 })
-                 // .on('mouseover', tip.show)
-                 // .on('mouseout', tip.hide);
-
-
-                 // .on("click", function(i){
-                 //   console.log(i.id);
-                 // })
-
+        if (score_average) {
+          //If value exists...
+          return color(score_average);
+        } else {
+          //If value is undefined...
+          return "#ccc";
+        }
+      })
+      .on("mouseover", function(e) {
+        d3.select(this).classed("highlight", true);
+        tip.show(e)
+      })
+      .on("mouseout", function(e) {
+        d3.select(this).classed("highlight", false);
+        tip.hide(e)
+      })
   });
-
-
-//Load in GeoJSON data
-
-  //Bind data and create one path per GeoJSON feature
-
 });
